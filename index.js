afabf021b858d61562ec7da92be76d27d7784a70
thaemorais <109257207+thaemorais@users.noreply.github.com>
2022-12-07 20:06:53 +0000

    Entrada de dados

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var salario_bruto = 7500;
+const prompt = require('prompt-sync')();
+
+var nome = prompt("Digite seu nome: ");
+var salario_bruto = parseFloat(prompt("Digite seu salario bruto: "));
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
-console.log("Salário Líquido: R$", salario_liquido, ",00")
+console.log("Salário Líquido: R$",salario_liquido,",00")
